0dbb5219888a761b949356e76431f5c76ec6e29c
tjclawson <tjamesclawson@gmail.com>
2020-05-01 16:10:07 -0400

    Add @Repository annotation

--- springy/src/main/java/com/tjclawson/springy/dao/FakePersonDataAccessService.java
+++ springy/src/main/java/com/tjclawson/springy/dao/FakePersonDataAccessService.java
@@ -1,11 +1,13 @@
 package com.tjclawson.springy.dao;
 
 import com.tjclawson.springy.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
