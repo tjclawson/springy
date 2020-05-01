7409ff869d8f276383eafa8988ffc0d6e8300e65
tjclawson <tjamesclawson@gmail.com>
2020-05-01 16:10:50 -0400

    Add @Service annotation and autowire person service

---
+++ springy/src/main/java/com/tjclawson/springy/service/PersonService.java
@@ -0,0 +1,22 @@
+package com.tjclawson.springy.service;
+
+import com.tjclawson.springy.dao.PersonDao;
+import com.tjclawson.springy.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+}
