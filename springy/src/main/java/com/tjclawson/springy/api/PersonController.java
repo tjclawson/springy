c1433d854312d98a2db49d783f1c58355ed434b3
tjclawson <tjamesclawson@gmail.com>
2020-05-01 16:20:30 -0400

    Add PersonController

---
+++ springy/src/main/java/com/tjclawson/springy/api/PersonController.java
@@ -0,0 +1,26 @@
+package com.tjclawson.springy.api;
+
+import com.tjclawson.springy.model.Person;
+import com.tjclawson.springy.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/V1/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
+        personService.addPerson(person);
+    }
+}
