ec3d7e2c2ea69ee632b778a00121db7b42965c25
tjclawson <tjamesclawson@gmail.com>
2020-05-01 15:35:18 -0400

    Init project using start.spring.io

---
+++ springy/src/test/java/com/tjclawson/springy/SpringyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjclawson.springy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
