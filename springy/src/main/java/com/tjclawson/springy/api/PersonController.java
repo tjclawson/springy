374def2b06d5be750ad26bb3289c4078c68c1995
tjclawson <tjamesclawson@gmail.com>
2020-05-01 16:23:56 -0400

    Add endpoint to retrieve all people

--- springy/src/main/java/com/tjclawson/springy/api/PersonController.java
+++ springy/src/main/java/com/tjclawson/springy/api/PersonController.java
@@ -3,10 +3,9 @@ package com.tjclawson.springy.api;
 import com.tjclawson.springy.model.Person;
 import com.tjclawson.springy.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/V1/person")
 @RestController
@@ -22,5 +21,10 @@ public class PersonController {
     @PostMapping
     public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
     }
 }
--- springy/src/main/java/com/tjclawson/springy/dao/PersonDao.java
+++ springy/src/main/java/com/tjclawson/springy/dao/PersonDao.java
@@ -2,6 +2,7 @@ package com.tjclawson.springy.dao;
 
 import com.tjclawson.springy.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -12,4 +13,6 @@ public interface PersonDao {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
 }
